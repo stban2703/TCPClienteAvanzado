a62cf840f11614aa7ee7506c1d995ae434fd27b4
stban2703 <s.t.ban1401@gmail.com>
2020-03-10 11:58:07 -0500

    Cambio pequeño

--- app/src/main/java/com/example/tcpclientavanzado/ComunicacionTCP.java
+++ app/src/main/java/com/example/tcpclientavanzado/ComunicacionTCP.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ComunicacionTCP extends Thread {
